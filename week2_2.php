9961ba622dc386e6e294dbebdefdb8f3ff403070
Theovanderjagt <112871814+Theovanderjagt@users.noreply.github.com>
2023-01-27 14:57:48 +0100

    2_2

---
+++ week2_2.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head> 
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Hello World</title>
+        </head>
+        <body>
+            <?php echo "Hello World";?>
+            <br><?php
+            echo "// = dit is een comment voor een php regel.";
+            ?>
+            <br>
+            <?php
+            echo "# = dit is een comment voor een php regel.";
+            ?>
+            <br>
+            <?php
+            echo "/* = dit is een comment voor meerdere php regels.";
+            ?>
+            <br>
+            <?php
+            echo " Parse error: syntax error, unexpected end of file = een error die verschijnt als iets niet goed afgesloten is."?>
+            </body>
+            </html>
+
