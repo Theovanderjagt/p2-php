58c6235ac0daac7a0e7b03577b83524741487dbc
Theovanderjagt <112871814+Theovanderjagt@users.noreply.github.com>
2023-02-03 12:09:28 +0100

    opdracht 5

---
+++ week5.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+        <input type="text" placeholder = " voornaam"> <br> <input type="text" placeholder = "achternaam">
+        <br>
+        
+   
+    <div>
+        <input type="radio" id = "man">
+        <label for="man"> man</label>
+    </div>
+   
+    <div>
+        <input type="radio" id = "vrouw">
+        <label for="vrouw">vrouw</label>
+    </div>
+    <input type="text" placeholder = "straat"> <br> <input type="number" placeholder = "huisnummer">
+    <input type="text" placeholder ="stad">
+   
+    <div>
+        <label for="country-select"></label>
+        <select name="country" id="country-select">
+            <option value="">country</option>
+            <option value="Nederland">Nederland</option>
+            <option value="Duitsland">Duitsland</option>
+            <option value="België">België</option>
+            <option value="Frankrijk">Frankrijk</option>
+        </select>
+    </div>
+ 
+    <input type="email" placeholder = "email">
+    <input type="password" placeholder = "wachtwoord">
+    <button>send</button>
+</body>
+</html>
+
+
+
